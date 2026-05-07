d0a708a96ef6db778a501deb45a910e1790e90eb
felixxvargas <felixvgiles@gmail.com>
2026-05-06 23:38:40 -0700

    fix: add 'use client' to explore [tab] page

--- app/(app)/explore/[tab]/page.tsx
+++ app/(app)/explore/[tab]/page.tsx
@@ -1,11 +1,7 @@
+'use client';
 import { Suspense } from 'react';
-import { redirect } from 'next/navigation';
 import { Explore } from '@/app/pages/Explore';
 
-const VALID_TABS = new Set(['posts', 'users', 'games', 'groups']);
-
-export default async function Page({ params }: { params: Promise<{ tab: string }> }) {
-  const { tab } = await params;
-  if (!VALID_TABS.has(tab)) redirect('/explore');
+export default function Page() {
   return <Suspense><Explore /></Suspense>;
 }
